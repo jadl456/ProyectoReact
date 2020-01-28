9c223e13c63e68b22d8cddf240864213460aceb3
jadl456 <jadl1997@gmail.com>
2020-01-28 16:52:09 -0500

    new
    new

--- src/index.js
+++ src/index.js
@@ -5,12 +5,13 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 import './global.css';
 import BadgeNew from './page/BadgeNew';
+import Badges from './page/Badges';
 
 //const element = React.createElement('h1',{},'Hola asdasd')
 
 const container = document.getElementById('app');
 
-ReactDOM.render(<BadgeNew />, container);
+ReactDOM.render(<Badges />, container);
 
 
 //firstName="Jonathan" lastName="Delgado" />, container);
---
+++ src/page/Badges.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+class Badges extends React.Component {
+    render(){
+        return 'Badges';
+    }
+}
+export default Badges;
